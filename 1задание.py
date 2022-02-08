58fdaa975f610fb37e2b6e04494e26133c40ae4c
irinkakiv <96539632+irinkakiv@users.noreply.github.com>
2022-02-08 23:36:44 +0300

    Delete 1задание.py

--- 1задание.py
+++
@@ -1,8 +0,0 @@
-x = 20
-y = 45
-print("Переменные x и y - ", x, y)
-string1 = input("Введите имя ")
-number1 = int(input("Введите первое число "))
-string2 = input("Введите второе имя ")
-number2 = int(input("Введите второе число "))
-print("Введеные значения - %10s %5d %10s %5d" % (string1, number1, string2, number2))
